3c93ed5c3f889e75a99d141409912b7e9271c17c
Amonvix <hspedroso@gmail.com>
2025-06-19 11:18:55 -0300

    IaC ready to launchgit add  .!

--- task_definitions.tf
+++ task_definitions.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_task_definition" "app_task" {
   container_definitions = jsonencode([
     {
       name      = "ecs-java-crud"
-      image = "<account_id>.dkr.ecr.us-east-1.amazonaws.com/<nome>:latest"
+      image = "\${var.account_id}.dkr.ecr.us-east-1.amazonaws.com/ecs-java-crud:latest"
       cpu       = 128
       memory    = 256
       essential = true
@@ -23,14 +23,14 @@ resource "aws_ecs_task_definition" "app_task" {
     },
     {
       name      = "titanic-fastapi-ecs"
-      image = "<account_id>.dkr.ecr.us-east-1.amazonaws.com/<nome>:latest"
+      image = "\${var.account_id}.dkr.ecr.us-east-1.amazonaws.com/titanic-fastapi-ecs:latest"
       cpu       = 64
       memory    = 128
       essential = false
     },
     {
       name      = "crud-ecs-python"
-      image = "<account_id>.dkr.ecr.us-east-1.amazonaws.com/<nome>:latest"
+      image = "\${var.account_id}.dkr.ecr.us-east-1.amazonaws.com/crud-ecs-python:latest"
       cpu       = 128
       memory    = 256
       essential = false
